1bb4b1dd74a36158b4c83c3dfc12d2e3afdbf260
Pranay <pranaytejais7@gmail.com>
2026-05-15 19:20:59 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ hamClose.addEventListener("click", () => {
     hamMenu.classList.toggle("hamMenuTransition")
 })
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+});
