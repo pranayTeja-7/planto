53b8a2388b121ad612471ea82ff286bf2d892256
Pranay <pranaytejais7@gmail.com>
2026-06-01 11:59:03 +0530

    slider

--- js/script.js
+++ js/script.js
@@ -9,8 +9,27 @@ headerHam.addEventListener("click", () => {
 })
 
 hamClose.addEventListener("click", () => {
-    hamMenu.classList.toggle("hamMenuTransition")
+    hamMenu.classList.toggle("hamMenuTransition");
+    hamMenu_plants_type_menu.classList.remove("plants-type-menu-transition")
 })
+
+
+
+
+let hamMmenu_plantsType = document.querySelector(".hamMenu .plantsType");
+let hamMenu_plants_type_menu = document.querySelector(".hamMenu .plantsType .plants-type-menu");
+let hamMenu_plants_type_close = document.querySelector(".hamMenu .plantsType .plants-type-menu .plants-close");
+
+hamMmenu_plantsType.addEventListener("click", () => {
+  hamMenu_plants_type_menu.classList.add("plants-type-menu-transition")
+})
+
+hamMenu_plants_type_close.addEventListener("click", (event) => {
+  event.stopPropagation();
+  hamMenu_plants_type_menu.classList.remove("plants-type-menu-transition");
+})
+
+
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
@@ -29,4 +48,7 @@ const swiper = new Swiper('.swiper', {
   },
 
   // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
